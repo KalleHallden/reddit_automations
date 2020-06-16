7db0d287eb2a07278d21378f785e7b96a10230f7
kalle hålldén <kallehalldeen@gmail.com>
2020-06-16 19:22:39 +0200

    Network Monitor created

--- net-listen.py
+++ net-listen.py
@@ -1,11 +1,12 @@
 import sys
 import subprocess 
 import os
+from decouple import config
 
-ip = "192.168.0.255"
-device = "192.168.0.28:"
+IP_NETWORK = config('IP_NETWORK')
+IP_DEVICE = config('IP_DEVICE')
 
-proc = subprocess.Popen(["ping", ip],stdout=subprocess.PIPE)
+proc = subprocess.Popen(["ping", IP_NETWORK],stdout=subprocess.PIPE)
 while True:
   line = proc.stdout.readline()
   if not line:
@@ -13,5 +14,5 @@ while True:
   #the real code does filtering here
   connected_ip = line.decode('utf-8').split()[3]
 
-  if connected_ip == device:
+  if connected_ip == IP_DEVICE:
       subprocess.Popen(["say", "Kalle just connected to the network"])
